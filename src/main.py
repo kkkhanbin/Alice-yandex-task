1f48453540d4137bec73ccaba8bcc11762eee06d
khanbin <totoshka1588@gmail.com>
2022-03-18 14:37:25 +0600

    Подготовка к развертыванию приложения на Heroku: была изменена настройка порта приложения

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import os
+
 from src import app
 
 
 def main():
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
